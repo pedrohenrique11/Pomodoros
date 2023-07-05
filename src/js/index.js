924a647dda3ad7a2f35b256c56418947c31598fc
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2023-07-05 17:12:22 +0000

    feat: add new time options

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,21 @@
 // Variaveis
+//buttons
 const startButton = document.querySelector('#startButton')
 const stopButton = document.querySelector('#stopButton')
+const pomodoroButton = document.querySelector('#pomodoroButton')
+const breakButton = document.querySelector('#breakButton')
+const longBreakButton = document.querySelector('#longBreakButton')
+
+// display
 const secondsDisplay = document.querySelector('#seconds')
 const minutesDisplay = document.querySelector('#minutes')
 
+//valores
 const seconds = parseInt(secondsDisplay.textContent)
 const minutes = parseInt(minutesDisplay.textContent)
 
 let countDown;
+
 
 // Eventos
 startButton.addEventListener('click', () => {
@@ -19,6 +27,20 @@ startButton.addEventListener('click', () => {
     toggleButtons()
 })
 stopButton.addEventListener('click', stopCount)
+
+pomodoroButton.addEventListener('click', () => {
+    minutesDisplay.textContent = 25;
+    secondsDisplay.textContent = '00';
+})
+breakButton.addEventListener('click', () => {
+    minutesDisplay.textContent = '05';
+    secondsDisplay.textContent = '00';
+})
+longBreakButton.addEventListener('click', () => {
+    minutesDisplay.textContent = 15;
+    secondsDisplay.textContent = '00';
+})
+
 
 // funçoes
 function resetDisplay() {
@@ -45,7 +67,10 @@ function startCount(duration, seconds, minutes) {
         if(duration >= 0 ) {
             startCount(duration, seconds, minutes)
         }
-        else {resetDisplay()}
+        else {
+            resetDisplay()
+            toggleButtons()
+        }
     }, 1000)
 }
 
