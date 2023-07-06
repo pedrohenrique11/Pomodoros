0a752045699d574ebeed4a49929d3d856dad1473
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2023-07-06 04:32:21 +0000

    style: new style

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,18 @@
-// Variaveis
-//buttons
 const startButton = document.querySelector('#startButton')
 const stopButton = document.querySelector('#stopButton')
 const pomodoroButton = document.querySelector('#pomodoroButton')
 const breakButton = document.querySelector('#breakButton')
 const longBreakButton = document.querySelector('#longBreakButton')
 
-// display
 const secondsDisplay = document.querySelector('#seconds')
 const minutesDisplay = document.querySelector('#minutes')
 
-//valores
 const seconds = parseInt(secondsDisplay.textContent)
 const minutes = parseInt(minutesDisplay.textContent)
 
 let countDown;
 
 
-// Eventos
 startButton.addEventListener('click', () => {
     const seconds = parseInt(secondsDisplay.textContent)
     const minutes = parseInt(minutesDisplay.textContent)
@@ -32,23 +27,28 @@ pomodoroButton.addEventListener('click', () => {
     minutesDisplay.textContent = 25;
     secondsDisplay.textContent = '00';
 
-    stopCount()
+    clearTimeout(countDown)
+    startButton.classList.remove('hide')
+    stopButton.classList.add('hide')
 })
 breakButton.addEventListener('click', () => {
     minutesDisplay.textContent = '05';
     secondsDisplay.textContent = '00';
-
-    stopCount()
+    
+    clearTimeout(countDown)
+    startButton.classList.remove('hide')
+    stopButton.classList.add('hide')
 })
 longBreakButton.addEventListener('click', () => {
     minutesDisplay.textContent = 15;
     secondsDisplay.textContent = '00';
 
-    stopCount()
+    clearTimeout(countDown)
+    startButton.classList.remove('hide')
+    stopButton.classList.add('hide')
 })
 
 
-// funçoes
 function resetDisplay() {
     minutesDisplay.textContent = minutes < 10 ? '0' + minutes : minutes;
     secondsDisplay.textContent = seconds < 10 ? '0' + seconds : seconds;
